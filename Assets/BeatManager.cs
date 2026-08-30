4ec4db24566502a1872d6f0768db518dd220b3e7
uitarboy001 <86054986+uitarboy001@users.noreply.github.com>
2026-08-30 14:55:46 +0700

    Initial commit

---
+++ Assets/BeatManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class BeatManager : MonoBehaviour
+{
+    [Header("Settings")]
+    public float bpm = 120f;
+    public float timingWindow = 0.15f; // ช่วงเวลากดติด (วินาที)
+
+    private float beatInterval;
+    private float nextBeatTime;
+    private bool hasInputThisBeat = false;
+
+    void Start()
+    {
+        beatInterval = 60f / bpm;
+        nextBeatTime = (float)AudioSettings.dspTime + beatInterval;
+    }
+
+    void Update()
+    {
+        // เมื่อข้ามเข้าสู่บีทถัดไป
+        if (AudioSettings.dspTime >= nextBeatTime)
+        {
+            if (!hasInputThisBeat)
+            {
+                Debug.Log("<color=red>[MISS] </color>");
+            }
+
+            nextBeatTime += beatInterval;
+            hasInputThisBeat = false;
+        }
+
+        // เช็กการกดปุ่ม Spacebar
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (hasInputThisBeat) return;
+
+            hasInputThisBeat = true;
+            float currentDsp = (float)AudioSettings.dspTime;
+
+            float diffPrevious = Mathf.Abs(currentDsp - (nextBeatTime - beatInterval));
+            float diffNext = Mathf.Abs(currentDsp - nextBeatTime);
+            float minDiff = Mathf.Min(diffPrevious, diffNext);
+
+            if (minDiff <= timingWindow)
+            {
+                Debug.Log("<color=green>[PERFECT PARRY] </color>");
+            }
+            else
+            {
+                Debug.Log("<color=red>[MISS] </color>");
+            }
+        }
+    }
+}
